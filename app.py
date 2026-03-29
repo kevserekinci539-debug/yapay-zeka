e400574aca03bec6d153aab5010ff62567bacd15
kevserekinci539-debug <kevserekinci539@gmail.com>
2026-03-29 21:41:48 +0300

    Update generative model and fix imports

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.title("Kişisel Yapay Zeka Botuma Hoş Geldin! 💬")
 # YENİ API Anahtarını Buraya Yaz
 GOOGLE_API_KEY = "AIzaSyB_dORdtYw3A65trWq_cH0M4CPhCOLD7n8"
 genai.configure(api_key=GOOGLE_API_KEY)
-model = genai.GenerativeModel('gemini-1.5-flash-latest')
+model = genai.GenerativeModel('gemini-pro')
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
